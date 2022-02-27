b957af7066c4b150137cb8ef14f139637422f938
Vivek C S <vcs@dtime.ai>
2022-02-27 21:53:28 +0530

    feat: add loader to serch results table

--- pages/search/components/result_table.tsx
+++ pages/search/components/result_table.tsx
@@ -1,11 +1,34 @@
+import ErrorAlert from "components/UI/alerts/error_alert";
+import Loader from "components/UI/loader";
+import { useContext } from "react";
+import { SearchVMContext } from "../search.vm";
 import TableBody from "./table_body";
 import TableHeader from "./table_header";
 
 const ResultTable = () => {
-  // 2fr repeat(5, minmax(200px, 1fr))
+  const vm = useContext(SearchVMContext);
+  if (vm.error) {
+    return (
+      <div className="w-full flex items-start justify-center">
+        <ErrorAlert
+          className="max-w-xl mx-auto"
+          message="Something went wrong while fetching datasets. Please try again later."
+        />
+      </div>
+    );
+  }
+
+  if (vm.isLoading) {
+    return (
+      <div className="h-[calc(100vh-var(--nav-height))]  w-full flex items-center justify-center">
+        <Loader />
+      </div>
+    );
+  }
+
   return (
-    <div className="overflow-x-auto">
-      <div className="grid gap-px [grid-template-columns:2fr_repeat(5,120px)] p-px w-full">
+    <div className="overflow-x-auto w-full">
+      <div className="grid gap-px [grid-template-columns:2fr_repeat(5,120px)] p-px">
         <TableHeader />
         <TableBody />
       </div>
