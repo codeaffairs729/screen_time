09dd1ece1eb370c6fb48b4f799a84ac788a0f4d1
Vivek C S <vcs@dtime.ai>
2022-06-08 18:32:56 +0530

    feat: add description to download urls

--- components/dataset/dataset_download.tsx
+++ components/dataset/dataset_download.tsx
@@ -4,64 +4,71 @@ import Dataset from "models/dataset.model";
 import Image from "next/image";
 
 const DatasetDownload = ({
-  dataset,
-  className = "",
+    dataset,
+    className = "",
 }: {
-  dataset: Dataset;
-  className?: string;
+    dataset: Dataset;
+    className?: string;
 }) => {
-  const urls = dataset.urls;
-  return (
-    <div className={clsx("flex", className)}>
-      <span className="text-xs font-medium text-gray-800 mr-1">Data:</span>
-      <div>
-        {urls && urls.length > 0 ? (
-          urls.map((url, i) => (
-            <DatasetDownloadItem
-              key={i}
-              dataset={dataset}
-              label={`${url.format}, ${url.sizemb} MB`}
-              url={url.url}
-            />
-          ))
-        ) : (
-          <span className="text-xs block text-gray-600">None</span>
-        )}
-      </div>
-    </div>
-  );
+    const urls = dataset.urls;
+    return (
+        <div className={clsx("flex", className)}>
+            <span className="text-xs font-medium text-gray-800 mr-1">
+                Data:
+            </span>
+            <div>
+                {urls && urls.length > 0 ? (
+                    urls.map((url, i) => (
+                        <DatasetDownloadItem
+                            key={i}
+                            dataset={dataset}
+                            label={`${
+                                url.description.replace(/['"]+/g, "")
+                                    ? url.description.replace(/['"]+/g, "") +
+                                      ": "
+                                    : ""
+                            }${url.format}, ${url.sizemb} MB`}
+                            url={url.url}
+                        />
+                    ))
+                ) : (
+                    <span className="text-xs block text-gray-600">None</span>
+                )}
+            </div>
+        </div>
+    );
 };
 
 const DatasetDownloadItem = ({
-  label,
-  dataset,
-  url,
+    label,
+    dataset,
+    url,
 }: {
-  label: string;
-  dataset: Dataset;
-  url: string;
+    label: string;
+    dataset: Dataset;
+    url: string;
 }) => {
-  const updateDownloadStat = () => {
-    Http.post(`/v1/datasets/${dataset.id}/downloads`);
-  };
-  const newUrl = url?.replace(/["']/g, "");
+    const updateDownloadStat = () => {
+        Http.post(`/v1/datasets/${dataset.id}/downloads`);
+    };
+    const newUrl = url?.replace(/["']/g, "");
 
-  return (
-    <a
-      href={newUrl}
-      className="flex space-x-1"
-      onClick={updateDownloadStat}
-      download
-    >
-      <Image
-        src="/images/icons/folder.svg"
-        width="20px"
-        height="20px"
-        alt="folder icon"
-      />
-      <span className="text-xs font-medium underline">{label}</span>
-    </a>
-  );
+    return (
+        <a
+            href={newUrl}
+            className="flex space-x-1"
+            onClick={updateDownloadStat}
+            download
+        >
+            <Image
+                src="/images/icons/folder.svg"
+                width="20px"
+                height="20px"
+                alt="folder icon"
+            />
+            <span className="text-xs font-medium underline">{label}</span>
+        </a>
+    );
 };
 
 export default DatasetDownload;
--- models/dataset.model.ts
+++ models/dataset.model.ts
@@ -1,141 +1,145 @@
 import { DateTime } from "luxon";
 
 class Dataset {
-  constructor({
-    id,
-    detail,
-    owner,
-    urls,
-  }: {
+    constructor({
+        id,
+        detail,
+        owner,
+        urls,
+    }: {
+        id: number;
+        detail: DatasetDetail;
+        owner: DatasetOwner;
+        urls: DatasetUrl[];
+    }) {
+        this.id = id;
+        this.detail = detail;
+        this.owner = owner;
+        this.urls = urls;
+    }
+
     id: number;
     detail: DatasetDetail;
     owner: DatasetOwner;
     urls: DatasetUrl[];
-  }) {
-    this.id = id;
-    this.detail = detail;
-    this.owner = owner;
-    this.urls = urls;
-  }
 
-  id: number;
-  detail: DatasetDetail;
-  owner: DatasetOwner;
-  urls: DatasetUrl[];
-
-  static fromJson(json: { [key: string]: any }) {
-    // if (!json) return null;
-    let topics = json["dataset"]["topics"];
-    if (typeof topics === "string") {
-      // TODO: Remove this once api is fixed and topics is actually an array
-      topics = topics.replace(/'/g, '"');
-      topics = JSON.parse(topics);
+    static fromJson(json: { [key: string]: any }) {
+        // if (!json) return null;
+        let topics = json["dataset"]["topics"];
+        if (typeof topics === "string") {
+            // TODO: Remove this once api is fixed and topics is actually an array
+            topics = topics.replace(/'/g, '"');
+            topics = JSON.parse(topics);
+        }
+        return new Dataset({
+            id: Number(json["id"]),
+            detail: {
+                domain: json["dataset"]["domain"],
+                hostUrl: json["dataset"]["hostURL"],
+                hostName: json["dataset"]["hostName"],
+                name: json["dataset"]["name"],
+                description: json["dataset"]["description"],
+                lastUpdate: DateTime.fromISO(json["dataset"]["lastupdate"]),
+                // license: json["dataset"]["licence"],
+                license: {
+                    type: json["dataset"]["licence"]["type"],
+                    version: json["dataset"]["licence"]["version"],
+                },
+                topics: topics,
+                keywords: json["dataset"]["keywords"],
+                locations: json["dataset"]["locations"],
+                dataQuality: json["dataset"]["dataquality"],
+                popularity: json["dataset"]["popularity"],
+                isLiked: json["dataset"]["userlike"],
+                isDisliked: json["dataset"]["userunlike"],
+                isFavourited: json["dataset"]["userfavourite"],
+                likes: json["dataset"]["like_count"],
+                dislikes: json["dataset"]["unlike_count"],
+                favourites: json["dataset"]["favourite_count"],
+                lastDownloaded: DateTime.fromISO(
+                    json["dataset"]["lastdownloaded"]
+                ),
+                displays: json["dataset"]["displays"],
+                views: json["dataset"]["views"],
+                downloads: json["dataset"]["downloads"],
+                sentiment: json["dataset"]["sentiment"],
+            },
+            owner: {
+                name: json["owner"]["name"],
+                organisation: json["owner"]["organisation"],
+                contact: {
+                    email: json["owner"]["contact"]["email"],
+                    phone: json["owner"]["contact"]["phone"],
+                    twitter: json["owner"]["contact"]["twitter"],
+                },
+                relatedOrganizations: json["owner"]["relatedOrganizations"],
+            },
+            urls: json["urls"].map((url: { [key: string]: any }) => ({
+                description: url["description"],
+                type: url["type"],
+                format: url["format"],
+                version: url["version"],
+                sizemb: url["sizemb"],
+                url: url["url"],
+            })),
+        });
     }
-    return new Dataset({
-      id: Number(json["id"]),
-      detail: {
-        domain: json["dataset"]["domain"],
-        hostUrl: json["dataset"]["hostURL"],
-        hostName: json["dataset"]["hostName"],
-        name: json["dataset"]["name"],
-        description: json["dataset"]["description"],
-        lastUpdate: DateTime.fromISO(json["dataset"]["lastupdate"]),
-        // license: json["dataset"]["licence"],
-        license: {
-          type: json["dataset"]["licence"]["type"],
-          version: json["dataset"]["licence"]["version"],
-        },
-        topics: topics,
-        keywords: json["dataset"]["keywords"],
-        locations: json["dataset"]["locations"],
-        dataQuality: json["dataset"]["dataquality"],
-        popularity: json["dataset"]["popularity"],
-        isLiked: json["dataset"]["userlike"],
-        isDisliked: json["dataset"]["userunlike"],
-        isFavourited: json["dataset"]["userfavourite"],
-        likes: json["dataset"]["like_count"],
-        dislikes: json["dataset"]["unlike_count"],
-        favourites: json["dataset"]["favourite_count"],
-        lastDownloaded: DateTime.fromISO(json["dataset"]["lastdownloaded"]),
-        displays: json["dataset"]["displays"],
-        views: json["dataset"]["views"],
-        downloads: json["dataset"]["downloads"],
-        sentiment: json["dataset"]["sentiment"],
-      },
-      owner: {
-        name: json["owner"]["name"],
-        organisation: json["owner"]["organisation"],
-        contact: {
-          email: json["owner"]["contact"]["email"],
-          phone: json["owner"]["contact"]["phone"],
-          twitter: json["owner"]["contact"]["twitter"],
-        },
-        relatedOrganizations: json["owner"]["relatedOrganizations"],
-      },
-      urls: json["urls"].map((url: { [key: string]: any }) => ({
-        type: url["type"],
-        format: url["format"],
-        version: url["version"],
-        sizemb: url["sizemb"],
-        url: url["url"],
-      })),
-    });
-  }
 
-  static fromJsonList(jsonList: { [key: string]: any }[]) {
-    // if(!jsonList) return null;
-    return jsonList?.map(Dataset.fromJson);
-  }
+    static fromJsonList(jsonList: { [key: string]: any }[]) {
+        // if(!jsonList) return null;
+        return jsonList?.map(Dataset.fromJson);
+    }
 }
 
 export type DatasetLicense = {
-  type: string;
-  version: string;
+    type: string;
+    version: string;
 };
 
 export type DatasetDetail = {
-  domain: string;
-  hostUrl: string;
-  hostName: string;
-  name: string;
-  description: string;
-  lastUpdate: DateTime;
-  license: DatasetLicense;
-  topics: string[];
-  keywords: string[];
-  locations: string[];
-  dataQuality: number;
-  popularity: number;
-  isLiked: boolean;
-  isDisliked: boolean;
-  isFavourited: boolean;
-  likes: number;
-  dislikes: number;
-  favourites: number;
-  lastDownloaded: DateTime;
-  displays: number;
-  views: number;
-  downloads: number;
-  sentiment: string;
+    domain: string;
+    hostUrl: string;
+    hostName: string;
+    name: string;
+    description: string;
+    lastUpdate: DateTime;
+    license: DatasetLicense;
+    topics: string[];
+    keywords: string[];
+    locations: string[];
+    dataQuality: number;
+    popularity: number;
+    isLiked: boolean;
+    isDisliked: boolean;
+    isFavourited: boolean;
+    likes: number;
+    dislikes: number;
+    favourites: number;
+    lastDownloaded: DateTime;
+    displays: number;
+    views: number;
+    downloads: number;
+    sentiment: string;
 };
 
 export type DatasetOwner = {
-  name: string;
-  organisation: string;
-  contact: {
-    email: string;
-    phone: string;
-    twitter: string;
-  };
-  relatedOrganizations: string[];
+    name: string;
+    organisation: string;
+    contact: {
+        email: string;
+        phone: string;
+        twitter: string;
+    };
+    relatedOrganizations: string[];
 };
 
 export type DatasetUrl = {
-  type: string;
-  format: string;
-  version: string;
-  sizemb: string;
-  url: string;
+    type: string;
+    description: string;
+    format: string;
+    version: string;
+    sizemb: string;
+    url: string;
 };
 
 export default Dataset;
