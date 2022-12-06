7e9f6b35cd1d16fa63e885a86d7fdf04cfaa2911
Farzpal <farzpal@dtime.ai>
2022-12-06 15:15:06 +0530

    Fixed anchor descendent warning.

--- pages/workspace/components/notifications_section/notification_card.tsx
+++ pages/workspace/components/notifications_section/notification_card.tsx
@@ -18,49 +18,45 @@ const NotificationCard = ({
     const { notification_type, description, read_status, created_at } =
         notification;
     return (
-        <Link href={notificationActionUrl(notification)}>
-            <a target="_blank">
-                <div
-                    id={"workspace-notification-" + index}
-                    className={`${
-                        !read_status ? "bg-dtech-main-light" : "bg-neutral-100"
-                    } my-4 px-2.5 py-2 cursor-pointer flex flex-col items-end rounded-lg text-dtech-additional-dark min-h-[100px]`}
-                >
-                    <span className="flex items-center w-full">
-                        <a
-                            id={"workspace-notification-heading-" + index}
-                            className={clsx(
-                                "text-inherit block text-sm font-semibold mr-auto"
-                            )}
-                        >
-                            {formatHeading(notification_type)}
-                        </a>
-                        <span
-                            id={"workspace-notification-age-" + index}
-                            className="text-gray-600 text-sm"
-                        >
-                            {getNotificationAge(created_at)}
-                        </span>
-                        {!read_status && (
-                            <div
-                                id={"workspace-notification-dot-" + index}
-                                className="ml-3 h-3 w-3 bg-dtech-notification-alert-secondary rounded-full"
-                            />
+        <a href={notificationActionUrl(notification)} target="_blank">
+            <div
+                id={"workspace-notification-" + index}
+                className={`${
+                    !read_status ? "bg-dtech-main-light" : "bg-neutral-100"
+                } my-4 px-2.5 py-2 cursor-pointer flex flex-col items-end rounded-lg text-dtech-additional-dark min-h-[100px]`}
+            >
+                <span className="flex items-center w-full">
+                    <span
+                        id={"workspace-notification-heading-" + index}
+                        className={clsx(
+                            "text-inherit block text-sm font-semibold mr-auto"
                         )}
-                    </span>
-                    <div
-                        className={`text-gray-800 text-sm justify-between my-4 mr-auto w-1/2`}
                     >
-                        <span id={"workspace-notification-detail-" + index}>
-                            {getNotificationSubHeading(notification_type)}{" "}
-                        </span>
-                        <span className="italic font-medium">
-                            {description}
-                        </span>
-                    </div>
+                        {formatHeading(notification_type)}
+                    </span>
+                    <span
+                        id={"workspace-notification-age-" + index}
+                        className="text-gray-600 text-sm"
+                    >
+                        {getNotificationAge(created_at)}
+                    </span>
+                    {!read_status && (
+                        <div
+                            id={"workspace-notification-dot-" + index}
+                            className="ml-3 h-3 w-3 bg-dtech-notification-alert-secondary rounded-full"
+                        />
+                    )}
+                </span>
+                <div
+                    className={`text-gray-800 text-sm justify-between my-4 mr-auto w-1/2`}
+                >
+                    <span id={"workspace-notification-detail-" + index}>
+                        {getNotificationSubHeading(notification_type)}{" "}
+                    </span>
+                    <span className="italic font-medium">{description}</span>
                 </div>
-            </a>
-        </Link>
+            </div>
+        </a>
     );
 };
 
