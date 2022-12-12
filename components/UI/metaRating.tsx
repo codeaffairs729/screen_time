cc164752c50ca6acbdac0e639197e1477656f3ba
Farzpal <farzpal@dtime.ai>
2022-12-12 11:28:23 +0530

    Updated code for metaRating

--- components/UI/metaRating.tsx
+++ components/UI/metaRating.tsx
@@ -1,17 +1,29 @@
 import StarRating from "components/UI/star_rating";
 import InfoIcon from "components/UI/icons/info_icon";
+import clsx from "clsx";
 type MetaRatingProps = {
     dataQuality: number;
+    label?: String;
     displayContext?: String;
+    className?: String;
+    labelClass?: String;
+    starClassName?: String;
 };
-const MetaRating = ({ dataQuality, displayContext = "" }: MetaRatingProps) => {
+const MetaRating = ({
+    dataQuality,
+    displayContext = "",
+    label = "Metadata Quality",
+    className = "",
+    labelClass = "",
+    starClassName = "",
+}: MetaRatingProps) => {
     return (
         <div
             data-testid={displayContext}
-            className={"flex space-x-1 flex-col ml-4"}
+            className={clsx(className, "flex space-x-1 flex-col ml-4")}
         >
-            <span className="text-xs whitespace-nowrap relative">
-                <span className="text-m">Metadata Quality</span>
+            <span className="text-xs whitespace-nowrap relative mr-6">
+                <span className={clsx(labelClass, "text-m")}>{label}</span>
                 <InfoIcon
                     oldIcon={false}
                     iconClasses="absolute left-[100%] top-[-25%] ml-[1px] h-3.5 w-3.5 text-black"
@@ -20,7 +32,7 @@ const MetaRating = ({ dataQuality, displayContext = "" }: MetaRatingProps) => {
             </span>
             <span className="text-xs">
                 <StarRating
-                    starClassName="text-black h-4 w-4"
+                    starClassName={clsx(starClassName, "text-black h-4 w-4")}
                     rating={dataQuality}
                 />
             </span>
