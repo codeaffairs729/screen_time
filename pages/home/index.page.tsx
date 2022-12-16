801b55c8e5f835a0cf23dc9fa2298be9a1940e9b
Farzpal <farzpal@dtime.ai>
2022-12-16 11:29:32 +0530

    Updated the logo for website
    - Adjusted the alignment for nav bar components.

--- pages/home/index.page.tsx
+++ pages/home/index.page.tsx
@@ -11,12 +11,18 @@ const HomePage = () => {
         <DefaultLayout showLogo={false} showSearchBar={false}>
             <div className="h-[calc(100vh-var(--nav-height))] flex items-center justify-center mx-2">
                 <div className="max-w-3xl mx-auto flex flex-col items-center">
-                    <Image
-                        src="/images/logo/dtechtive_logo_strapline.png"
-                        width="315"
-                        height="155"
-                        alt="Dtechtive logo"
-                    />
+                    <span>
+                        <Image
+                            src="/images/logo/dtechtive_without_tagline.svg"
+                            width="315"
+                            height="155"
+                            alt="Dtechtive logo"
+                        />
+                        <p className="text-[#c1c1c1] text-[11px] font-medium select-none mt-[-10px]">
+                            Discovering the data that other search engines cannot
+                            reach
+                        </p>
+                    </span>
                     <div className="mt-6 max-w-xl mx-auto w-[575px]">
                         <DatasetSearchInput
                             onChange={(option) => {
