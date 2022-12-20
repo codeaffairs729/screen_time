d2e008f50c7280efcf85f50579d84ca9d810be7d
Farzpal <farzpal@dtime.ai>
2022-12-20 17:35:39 +0530

    Added no results found dialog for organisation search page.

--- pages/search/components/organization/components/organization_table.tsx
+++ pages/search/components/organization/components/organization_table.tsx
@@ -2,14 +2,36 @@ import { Key } from "react";
 import OrganizationResult from "./organization_result";
 import { useContext } from "react";
 import { OrganizationSearchVMContext } from "pages/search/organisation.vm";
+import NoResults from "../../no_results";
+import { useRouter } from "next/router";
 const OraganizationTable = () => {
+    const router = useRouter();
+    const {
+        query: { q },
+    } = router;
     const vm = useContext(OrganizationSearchVMContext);
-    console.log(vm.datasets);
+
+    if (!vm.datasets?.length) {
+        return (
+            <div className="w-full flex items-start justify-center">
+                <NoResults
+                    message={`No results found for ${q}.`}
+                    subMessages={[
+                        "Try different keywords.",
+                        "Make sure that all words are spelled correctly.",
+                    ]}
+                />
+            </div>
+        );
+    }
+
     return (
         <>
-            {vm.datasets?.map((dataset: { id: Key | null | undefined }, i: any) => (
-                <OrganizationResult key={dataset.id} dataset={dataset} />
-            ))}
+            {vm.datasets?.map(
+                (dataset: { id: Key | null | undefined }, i: any) => (
+                    <OrganizationResult key={dataset.id} dataset={dataset} />
+                )
+            )}
         </>
     );
 };
