85431da075596d6e379d8c8daa460418ddefdd15
Farzpal <farzpal@dtime.ai>
2023-01-12 18:43:40 +0530

    Added dummy data for dataQuality

--- pages/organisation/organisation_detail.vm.ts
+++ pages/organisation/organisation_detail.vm.ts
@@ -4,7 +4,7 @@ import { DateTime } from "luxon";
 import Organisation from "models/organisation.model";
 import { createContext, useState, Dispatch, SetStateAction } from "react";
 import toast from "react-hot-toast";
-
+import { getNotificationAge } from "pages/workspace/notification.vm";
 export enum insightTabIndex {
     dataset_quality,
     search_term,
@@ -156,13 +156,17 @@ const OrganisationDetailVM = (
 
     const fectchDataFileQuality = (ids: number[]) =>
         excuteFectchDataFileQuality(
-            () =>
-                Http.post("/v1/datasets/stats", {
-                    meta_dataset_ids: ids,
-                }),
+            () => {
+                // return  Http.post("/v1/datasets/stats", {meta_dataset_ids: ids,}),
+            },
             {
                 postProcess: (res) => {
                     return res;
+                },
+                onError: (e) => {
+                    toast.error(
+                        "Something went wrong while fetching organisation Data Quality insights."
+                    );
                 },
             }
         );
@@ -229,8 +233,8 @@ const OrganisationDetailVM = (
         organisation,
         organisationRankedDatasets,
         organisationDatasets,
-        metaDataQulaity,
-        dataFileQuality,
+        metaDataQulaity: metaFile,
+        dataFileQuality: dataFile,
         searchTerms: jsonToSearchTerms(SearchTerms),
         downloadMetrics: jsonToOrgDownloadMetrics(DownloadMetrics),
         isLoading,
@@ -303,7 +307,7 @@ const jsonToSearchTerms = (json: any): SearchTermType =>
     json.map((term: any) => ({
         title: term["title"],
         count: term["count"],
-        // lastUsed: DateTime.fromISO(term["updated_at"]),
+        lastUsed: getNotificationAge(term["created_at"]),
     }));
 
 const jsonToOrgDownloadMetrics = (json: any): DownloadMetrics => ({
@@ -427,4 +431,94 @@ const DownloadMetrics = {
         { name: "gov", value: 500 },
         { name: "Plan", value: 300 },
     ],
+};
+const rating = [
+    [{ 1: 10 }, { 2: 20 }, { 3: 30 }, { 4: 20 }, { 5: 10 }],
+    [{ 1: 10 }, { 2: 20 }, { 3: 50 }, { 4: 20 }, { 5: 10 }],
+    [{ 1: 50 }, { 2: 20 }, { 3: 30 }, { 4: 20 }, { 5: 10 }],
+    [{ 1: 10 }, { 2: 20 }, { 3: 30 }, { 4: 20 }, { 5: 10 }],
+    [{ 1: 20 }, { 2: 10 }, { 3: 20 }, { 4: 20 }, { 5: 10 }],
+    [{ 1: 10 }, { 2: 10 }, { 3: 20 }, { 4: 20 }, { 5: 10 }],
+];
+const datasets = [
+    {
+        id: 1,
+        title: "2011 Output Area code, old to new",
+        description:
+            "This file provides a look-up between the archived 2011 Output Area (OA) code (published 15 August 2013) and the new 2011 This file provides a look-up between the archived 2011 Output Area (OA) code (published 15 August",
+        rating: 10,
+    },
+    {
+        id: 2,
+        title: "2011 Output Area code, old to new",
+        description:
+            "This file provides a look-up between the archived 2011 Output Area (OA) code (published 15 August 2013) and the new 2011 This file provides a look-up between the archived 2011 Output Area (OA) code (published 15 August",
+        rating: 30,
+    },
+    {
+        id: 3,
+        title: "2011 Output Area code, old to new",
+        description:
+            "This file provides a look-up between the archived 2011 Output Area (OA) code (published 15 August 2013) and the new 2011 This file provides a look-up between the archived 2011 Output Area (OA) code (published 15 August",
+        rating: 20,
+    },
+];
+const dataFile: any = {
+    overall_score: {
+        title: "Overall score",
+        rating: rating,
+        datasets: datasets,
+    },
+    accuracy: {
+        title: "accuracy",
+        rating: rating,
+        datasets: datasets,
+    },
+    consistency: {
+        title: "consistency",
+        rating: rating,
+        datasets: datasets,
+    },
+    clarity: {
+        title: "clarity",
+        rating: rating,
+        datasets: datasets,
+    },
+    readiness: {
+        title: "readiness",
+        rating: rating,
+        datasets: datasets,
+    },
+};
+const metaFile: any = {
+    overall_score: {
+        title: "Overall score",
+        rating: rating,
+        datasets: datasets,
+    },
+    findability: {
+        title: "findability",
+        rating: rating,
+        datasets: datasets,
+    },
+    accessibility: {
+        title: "accessibility",
+        rating: rating,
+        datasets: datasets,
+    },
+    reusability: {
+        title: "reusability",
+        rating: rating,
+        datasets: datasets,
+    },
+    contextuality: {
+        title: "contextuality",
+        rating: rating,
+        datasets: datasets,
+    },
+    interoperability: {
+        title: "interoperability",
+        rating: rating,
+        datasets: datasets,
+    },
 };
