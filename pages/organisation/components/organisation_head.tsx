5e360a6e8da612bb9aa23fdb350a2e4256a7aa20
Farzpal <farzpal@dtime.ai>
2023-03-04 11:15:54 +0530

    updated round to ceil dataQuality

--- pages/organisation/components/organisation_head.tsx
+++ pages/organisation/components/organisation_head.tsx
@@ -40,7 +40,7 @@ const OrganisationHead = () => {
                     </div>
                     <div className="flex justify-between items-center">
                         <MetaRating
-                            dataQuality={Math.round(dataQuality) ?? 0}
+                            dataQuality={Math.ceil(dataQuality) ?? 0}
                             title="Estimated based on the EU Metadata Quality Assessment method."
                         />
                         {buttonTags?.map((tag: string, index: number) => (
