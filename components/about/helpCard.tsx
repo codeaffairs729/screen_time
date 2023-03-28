73d7faf24adfa0e0c2b8002a224de33619761fe0
farzpal <farzpal@dtime.ai>
2023-03-28 18:27:01 +0530

    Added youtube iframe in about section

--- components/about/helpCard.tsx
+++ components/about/helpCard.tsx
@@ -4,19 +4,15 @@ import { useState } from "react";
 
 const HelpCard = ({
     cardNumber,
-    image,
-    setImage,
     imageOne,
     imageTwo,
     title,
     shortDesc,
     desc,
     cardUrl,
-    cardUrlLabel
+    cardUrlLabel,
 }: {
-    cardNumber:number
-    image: boolean;
-    setImage: Function;
+    cardNumber: number;
     imageOne: StaticImageData;
     imageTwo: StaticImageData;
     title: string;
@@ -26,67 +22,74 @@ const HelpCard = ({
     cardUrlLabel: string;
 }) => {
     const [showDesc, setShowDesc] = useState(false);
+    const [image, setImage] = useState(true);
+
     return (
-        <div>
-            <div className=" shadow-lg w-max border-2 my-2 rounded-lg flex flex-col md:flex-row">
-                <div className=" mx-[10%] my-[4%] flex flex-col items-center">
-                    <div className="">
-                        <Image className=""
-                            src={image ? imageOne : imageTwo}
-                            height={1000}
-                            width={1000}
-                        />
-                    </div>
-                    <div className="flex flex-row">
-                        <div
-                            className={`w-4 h-4  m-3 rounded-full ${
-                                image ? `bg-dtech-main-dark` : `bg-[#DBCFE2]`
-                            }`}
-                            onClick={() => setImage(true)}
-                        ></div>
-                        <div
-                            className={`w-4 h-4  m-3 rounded-full ${
-                                image ? `bg-[#DBCFE2]` : `bg-dtech-main-dark`
-                            }`}
-                            onClick={() => setImage(false)}
-                        ></div>
-                    </div>
+        <div
+            className={`shadow-lg w-[100%] border-2 my-2 rounded-lg  flex flex-col ${
+                (cardNumber % 2 ==0) ? "md:flex-row" : "md:flex-row-reverse"
+            }`}
+        >
+            <div className=" my-[1%] flex flex-col items-center w-[100%] md:mx-16">
+                <div className="">
+                    <Image
+                        src={image ? imageOne : imageTwo}
+                        height={450}
+                        width={450}
+                    />
                 </div>
-                <div className=" mx-[10%] my-[4%] flex flex-col ">
-                    <div className=" shadow-md mx-1 my-2 text-center bg-inherit border-2 rounded-xl flex flex-col justify-between">
-                        <div>
-                            <div className="text-dtech-main-dark text-4xl my-6 ml-4 font-sans font-bold break-words">
-                                <span className=" text-dtech-main-light bold text-6xl mr-5">{cardNumber+1}</span>{title}
-                            </div>
-                            <div className="text-dtech-main-dark text-1xl my-6">
-                                {shortDesc}
-                            </div>
-                            {showDesc && (
-                                <div className="text-dtech-main-dark text-1xl my-10">
-                                    {desc.map((item, index) => (
-                                        <div key={index} className="flex justify-left items-center mx-8">
-                                            <div className="select-none outline-none  w-2 h-2 bg-black rounded-full mr-1"></div>
-                                            {item}
-                                        </div>
-                                    ))}
-                                </div>
-                            )}
+                <div className="flex flex-row">
+                    <div
+                        className={`w-4 h-4  m-3 rounded-full ${
+                            image ? `bg-dtech-main-dark` : `bg-[#DBCFE2]`
+                        }`}
+                        onClick={() => setImage(true)}
+                    ></div>
+                    <div
+                        className={`w-4 h-4  m-3 rounded-full ${
+                            image ? `bg-[#DBCFE2]` : `bg-dtech-main-dark`
+                        }`}
+                        onClick={() => setImage(false)}
+                    ></div>
+                </div>
+            </div>
+            <div className=" my-[4%] flex flex-col md:mx-20 ">
+                <div className=" shadow-md mx-1 my-2 text-center bg-inherit border-2 rounded-xl flex flex-col justify-between">
+                    <div>
+                        <div className="text-dtech-main-dark text-4xl my-6 ml-4 font-sans font-bold break-words">
+                            <span className=" text-dtech-main-light bold text-6xl mr-5">
+                                {cardNumber + 1}
+                            </span>
+                            {title}
                         </div>
-                        <div
-                            className=" bg-dtech-main-light text-dtech-main-dark text-lg font-sans p-5 cursor-pointer"
-                            onClick={() => setShowDesc(!showDesc)}
-                        >
-                            {showDesc ? (
-                                <div>Read Less</div>
-                            ) : (
-                                <div>Read More</div>
-                            )}
+                        <div className="text-dtech-main-dark text-1xl my-6">
+                            {shortDesc}
                         </div>
-                    </div>{" "}
-                    <div className="w-[420px] mx-1 my-5 rounded-full p-5 text-2xl text-white justify-between text-center bg-dtech-main-dark max-w-full font-bold underline">
-                                    
-                        <Link href={cardUrl}><a target="_blank">Try Dtechtive</a></Link>
+                        {showDesc && (
+                            <div className="text-dtech-main-dark text-1xl my-10">
+                                {desc.map((item, index) => (
+                                    <div
+                                        key={index}
+                                        className="flex justify-left  mx-8"
+                                    >
+                                        <div className="select-none outline-none  w-2 h-2 bg-black rounded-full mr-2 mt-[7px]"></div>
+                                        {item}
+                                    </div>
+                                ))}
+                            </div>
+                        )}
                     </div>
+                    <div
+                        className=" bg-dtech-main-light text-dtech-main-dark text-lg font-sans p-5 cursor-pointer"
+                        onClick={() => setShowDesc(!showDesc)}
+                    >
+                        {showDesc ? <div>Read Less</div> : <div>Read More</div>}
+                    </div>
+                </div>{" "}
+                <div className="w-[420px] mx-1 my-5 rounded-full p-5 text-2xl text-white justify-between text-center bg-dtech-main-dark max-w-full font-bold underline">
+                    <Link href={cardUrl}>
+                        <a target="_blank">{cardUrlLabel}</a>
+                    </Link>
                 </div>
             </div>
         </div>
