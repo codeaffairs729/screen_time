71e86aec866df51d5aac703f8077b706dc06d5d5
farzpal <farzpal@dtime.ai>
2023-08-09 19:06:26 +0530

    ui for histogram

--- pages/organisation/components/insights_section/quality_insights/section.tsx
+++ pages/organisation/components/insights_section/quality_insights/section.tsx
@@ -1,12 +1,32 @@
 import Accordian from "components/UI/accordian";
 import Table from "../../table";
 import MetaRating from "components/UI/metaRating";
-import BarGraph from "components/UI/BarGraph";
 import Loader from "components/UI/loader";
 import { useContext, useEffect } from "react";
 import { QualityMetricVMContext } from "./quality_metric.vm";
 import ErrorAlert from "components/UI/alerts/error_alert";
 import { OrganisationDetailVMContext } from "pages/organisation/organisation_detail.vm";
+// import { Bar } from "react-chartjs-2";
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend,
+} from "chart.js";
+import { AgChartsReact } from "ag-charts-react";
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+);
+
 
 const TABLE_HEADERS = ["Score", "Dataset"];
 
@@ -86,8 +106,8 @@ const QualityInsightsBody = () => {
                             label={getLabel(items[key].title)}
                             ratings={items[key].rating}
                             tooltipTitle={items[key].tooltipTitle}
-                            selectedLabel = {selectedLabel}
-                            orgQuality={(!organisation)? 0:organisation.dataQuality}
+                            selectedLabel={selectedLabel}
+                            orgQuality={(!organisation) ? 0 : organisation.dataQuality}
                         />
                     }
                     key={selectedLabel + key}
@@ -107,7 +127,67 @@ const QualityInsightsBody = () => {
                                     }
                                 />
                             </div>
-                            <BarGraph
+                            <div className="relative w-full">
+
+                                <div className=" ">
+                                    <AgChartsReact options={
+                                        {
+                                            data: getChartData(items[key].rating),
+                                            series: [{
+                                                xKey: 'star',
+                                                yKey: 'starRating',
+                                                type: 'column',
+                                                // type: 'histogram',
+                                                strokeWidth: 2,
+                                                stroke: '#3F0068',
+                                                fill:'#3F0068'
+
+                                            },
+                                            {
+                                                xKey: 'star',
+                                                yKey: 'starRating',
+                                                // type: 'column', // Bar series
+
+                                                strokeWidth: 1,
+                                                tooltip: {
+                                                    enabled: true
+                                                },
+                                                stroke: '#3F0068',
+                                                fill: '#3F0068'
+
+                                            }
+                                            ],
+                                            axes: [
+                                                {
+                                                    type: 'category',
+                                                    position: 'bottom',
+                                                    title: {
+                                                        text: 'Star',
+                                                    },
+                                                    label: {
+                                                        formatter: (params) => {
+                                                            const value = parseFloat(params.value);
+                                                            return value % 1 === 0 ? value.toString() : '';
+                                                        }
+                                                    }
+                                                },
+                                                {
+                                                    type: 'number',
+                                                    position: 'left',
+                                                    title: {
+                                                        text: 'Ratings',
+                                                    },
+                                                    tick: {
+                                                        interval:5
+                                                    }
+                                                },
+                                            ]
+                                                                                    }
+                                    } />
+                                </div>
+                                
+                            </div>
+                            {/* <BarGraph
                                 data={getRating(items[key].rating, index)}
                                 strokeWidthAxis={2}
                                 strokeWidthLabelList={0}
@@ -118,7 +198,7 @@ const QualityInsightsBody = () => {
                                 yvalue="Datasets"
                                 barDatakey={"rating"}
                                 labelListDatakey={"name"}
-                            />
+                            /> */}
                         </div>
                     ) : (
                         <div className="h-[calc(40vh-var(--nav-height))] w-full flex items-center justify-center">
@@ -143,11 +223,11 @@ const AccordianLabel = ({
     tooltipTitle: string;
     selectedLabel: number;
     orgQuality: number;
-    }) => {
+}) => {
     return (
         <MetaRating
             label={label}
-            dataQuality={(selectedLabel==1 && label == "OverallScore") ? orgQuality :getAvg(ratings, label)}
+            dataQuality={(selectedLabel == 1 && label == "OverallScore") ? orgQuality : getAvg(ratings, label)}
             className="!flex-row ml-0"
             labelClass="!text-lg text-dtech-dark-grey"
             starClassName="!w-6 !h-6 text-[#5F5F63]"
@@ -159,20 +239,22 @@ const AccordianLabel = ({
 const getAvg = (ratings: any, label: any) => {
     let ratingSum;
     let count;
-    if (ratings.length == 6) {
+    if (ratings.length == 11) {
         const ratingData = ratings.map(
-            (rate: any, index: number) => rate[index]
+            (rate: any, index: number) => {
+                return rate[index / 2]
+            }
         );
         count = ratingData.reduce(
             (accumulator: any, curValue: any) => accumulator + curValue,
             0
         );
 
-        ratingSum = ratingData.reduce(
-            (accumulator: any, curValue: any, curIndex: any) =>
-                accumulator + curValue * curIndex,
-            0
-        );
+        ratingSum = ratings.reduce((accumulator: any, curValue: any) => {
+            const rating = parseFloat(Object.keys(curValue)[0]);
+            const value = curValue[rating];
+            return accumulator + rating * value;
+        }, 0);
     } else {
         const ratingData = ratings.map(
             (rate: any, index: number) => rate[index + 1]
@@ -188,7 +270,6 @@ const getAvg = (ratings: any, label: any) => {
             0
         );
     }
-
     return ratingSum / count;
 };
 
@@ -196,8 +277,7 @@ const getLabel = (title: string) => {
     return `${title[0].toUpperCase()}${title.slice(1)}`;
 };
 
-const getTableData = (key: string, datasets: any) =>
-{
+const getTableData = (key: string, datasets: any) => {
     if (key === "overallScore") return datasets?.map((dataset: any, index: number) => {
         const datasetCell = (
             <DisplayDataset
@@ -214,10 +294,10 @@ const getTableData = (key: string, datasets: any) =>
         return datasets?.map((dataset: any, index: number) => {
             let rating;
             if (dataset.factorWiseRating == 0) {
-                rating=0
+                rating = dataset.rating
             }
             else {
-                rating=dataset.factorWiseRating[key]
+                rating = dataset.factorWiseRating[key]
             }
 
             const datasetCell = (
@@ -250,4 +330,15 @@ const getRating = (ratings: any, index: any) => {
     return rating;
 };
 
+const getChartData = (rating: any) => {
+    const data = rating.map((obj: any) => {
+        const key = parseFloat(Object.keys(obj)[0]);
+        const value = obj[key];
+        return {
+            star: ` ${key}`,
+            starRating: value
+        };
+    })
+    return data
+}
 export default QualityInsightsBody;
