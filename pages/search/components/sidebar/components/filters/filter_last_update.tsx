1d2b702cb63091ce39ad20c227104e6183c2893f
farzpal <farzpal@dtime.ai>
2023-08-16 11:39:51 +0530

    dynamic changes in code

--- pages/search/components/sidebar/components/filters/filter_last_update.tsx
+++ pages/search/components/sidebar/components/filters/filter_last_update.tsx
@@ -8,14 +8,18 @@ const FilterLastUpdate = () => {
   const [filterOptions, setFilterOptions] = useState<
         FilterOptionItem[] | undefined
     >([]);
-
   useEffect(() => {
-    const filterValues = [
-      { checkbox: false, label: "Past Day", value: "Past day" },
-      { checkbox: false, label: "Past Week", value: "Past 7 days" },
-      { checkbox: false, label: "Past Month", value: "Past 30 days" },
-      { checkbox: false, label: "Past 3 Months", value: "Past 90 days" },
-    ]
+    const filterValues = vm.filterOptions?.last_updated?.map((format: any) => ({
+      value: format.value,
+      label: format.count,
+      checkbox: false,
+    }));
+    // const filterValues = [
+    //   { checkbox: false, label: "Past Day", value: "Past day" },
+    //   { checkbox: false, label: "Past Week", value: "Past 7 days" },
+    //   { checkbox: false, label: "Past Month", value: "Past 30 days" },
+    //   { checkbox: false, label: "Past 3 Months", value: "Past 90 days" },
+    // ]
 
     setFilterOptions(filterValues);
   }, [vm.filterOptions]);
@@ -31,8 +35,8 @@ const FilterLastUpdate = () => {
         <FilterCheckboxField
           key={field.id}
           register={register(`last_updated.${i}.checkbox`)}
-          label={field.label}
-          value={field.value}
+          label={field.value}
+          value={field.label}
           defaultChecked={!!field.checkbox}
         />
       ))}
