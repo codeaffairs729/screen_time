9beff7acd42ae9e9368886f3f0b77fe8179619e2
“pearl” <pearl.sharma@dtime.ai>
2023-09-15 12:43:43 +0530

    Fixed ui changes

--- components/UI/new_result_card_action.tsx
+++ components/UI/new_result_card_action.tsx
@@ -9,6 +9,7 @@ import ReactTooltip from "react-tooltip";
 import clsx from "clsx";
 import Image from "next/image";
 import SourceArrow from "public/images/icons/source_arrow.svg";
+import SourceArrowWhite from "public/images/icons/source_arrow_white.svg";
 import CiteQuotes from "public/images/icons/cite_quote.svg";
 import { useRouter } from "next/router";
 
@@ -27,6 +28,12 @@ const NewResultCardAction = ({
     gridClass = "",
 }: NewResultCardActionProps) => {
     const [share, setShare] = useState(false);
+
+    const [shareClicked, setShareClicked] = useState<boolean>(false);
+    const [bookmarkClicked, setBookmarkClicked] = useState<boolean>(false);
+    const [favouriteClicked, setFavouriteClicked] = useState<boolean>(false);
+    const [arrowActive, setArrowActive] = useState<boolean>(false);
+
     const router = useRouter();
 
     if (!data) {
@@ -37,6 +44,18 @@ const NewResultCardAction = ({
     const handleClosePopup = () => {
         setShare(false);
     };
+
+    const shareColor = shareClicked
+        ? " !text-white !bg-dtech-main-dark"
+        : " !text-dtech-new-main-light ";
+
+    const bookmarkColor = bookmarkClicked
+        ? " !text-white !bg-dtech-main-dark "
+        : " !text-dtech-new-main-light";
+
+    const favouriteColor = favouriteClicked
+    ? " !text-white !bg-dtech-main-dark "
+    : " !text-dtech-new-main-light";
 
     return (
         <div className={clsx("flex", className)}>
@@ -53,17 +72,26 @@ const NewResultCardAction = ({
                                 <BsGlobe className="sm:h-6 sm:w-6 h-4 w-4 text-dtech-new-main-light cursor-pointer" />
                             </div>
                         ) : (
-                            <div className="hover:bg-[#6DCDCB] px-2 pt-2">
-                                <Image src={SourceArrow} className=" " />
+                            <div className="hover:bg-[#6DCDCB] active:bg-dtech-main-dark !hover:text-white px-2 pt-2 ml-1"
+                            onMouseDown={() => setArrowActive(true)}
+                            onMouseUp={() => setArrowActive(false)}
+                            onMouseLeave={() => setArrowActive(false)}
+                            >
+                                <Image src={arrowActive ? SourceArrowWhite :SourceArrow} className=" " />
                             </div>
                         )}
-                    </a>
-                    <div className="text-dtech-new-main-light w-max">
+                    <div className="text-dtech-main-dark underline underline-offset-2 cursor-pointer w-max">
                         {!href?.includes("dataset") ? "Website" : "Source"}
                     </div>
+                    </a>
                 </div>
                 <div className=" flex flex-col justify-center items-center">
-                    <div className="hover:bg-[#6DCDCB] active:bg-white px-2 pt-2 mb-[-10px]">
+                    <div
+                        className={` hover:bg-[#6DCDCB] px-2 pt-2 mb-[-10px] active:bg-dtech-main-dark !hover:text-white ${favouriteColor}`}
+                        onMouseDown={() => setFavouriteClicked(true)}
+                        onMouseUp={() => setFavouriteClicked(false)}
+                        onMouseLeave={() => setFavouriteClicked(false)}
+                    >
                         <FavouriteBtn
                             className="mx-auto"
                             datasetStats={data}
@@ -74,11 +102,17 @@ const NewResultCardAction = ({
                     <div className="text-dtech-new-main-light mt-2">Like</div>
                 </div>
                 <div className=" flex flex-col justify-center items-center">
-                    <div className="hover:bg-[#6DCDCB] px-2 pt-2 mb-[-10px]">
+                    <div
+                        className={`hover:bg-[#6DCDCB] px-2 pt-2 mb-[-10px]  active:bg-dtech-main-dark !hover:text-white ${bookmarkColor}`}
+                        onMouseDown={() => setBookmarkClicked(true)}
+                        onMouseUp={() => setBookmarkClicked(false)}
+                        onMouseLeave={() => setBookmarkClicked(false)}
+                    >
                         <BookmarkBtn
                             className="mx-auto"
                             data={data}
                             recordType={data.recordType}
+                            bookmarkColor={bookmarkColor}
                         />
                     </div>
                     <div className="text-dtech-new-main-light mt-2">Save</div>
@@ -88,9 +122,19 @@ const NewResultCardAction = ({
                         {({ open }) => (
                             <>
                                 <Menu.Button>
-                                    <div data-tip="Share on social media">
+                                    <div
+                                        data-tip="Share on social media"
+                                        className={`hover:bg-[#6DCDCB] text-dtech-main-light px-2 pt-2 pb-2 mb-[-10px]  ${shareColor}`}
+                                        onMouseDown={() =>
+                                            setShareClicked(true)
+                                        }
+                                        onMouseUp={() => setShareClicked(false)}
+                                        onMouseLeave={() =>
+                                            setShareClicked(false)
+                                        }
+                                    >
                                         <BsShareFill
-                                            className="sm:h-6 sm:w-6 h-4 w-4 text-dtech-new-main-light cursor-pointer"
+                                            className={`sm:h-6 sm:w-6 h-4 w-4  cursor-pointer`}
                                             data-modal-toggle="popup"
                                             onClick={() => setShare(true)}
                                         />
