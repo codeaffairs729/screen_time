199875ceae49c781f30fe9f4ddf1398adb943018
“pearl” <pearl.sharma@dtime.ai>
2023-10-23 23:05:55 +0530

    fix api issue

--- pages/workspace/components/lists_section/bookmarks_section/index.tsx
+++ pages/workspace/components/lists_section/bookmarks_section/index.tsx
@@ -5,7 +5,6 @@ const BookmarksSection = ({ list }: { list: any }) => {
     if (!list.listDatasets.length && !list.listOrganisations.length) {
         return (
             <div className="w-full flex items-center justify-center my-3">
-                <p>No datasets or organisations bookmarked here.</p>
                 <p>No datasets or providers bookmarked here.</p>
             </div>
         );
