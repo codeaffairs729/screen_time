1591a55ab3b0423bddaa525932e3e72845b4fead
“pearl” <pearl.sharma@dtime.ai>
2023-10-23 18:35:47 +0530

    Added pagination in Workspace

--- pages/workspace/components/lists_section/favourites_section.tsx
+++ pages/workspace/components/lists_section/favourites_section.tsx
@@ -1,12 +1,46 @@
 import ErrorAlert from "components/UI/alerts/error_alert";
 import InfoAlert from "components/UI/alerts/info_alert";
 import Loader from "components/UI/loader";
+import Pagination from "components/UI/pagination_for_datasets";
 import ResultCard from "components/UI/result_card";
+import { useRouter } from "next/router";
 import FavouriteVM from "pages/workspace/favourite.vm";
+import { useEffect, useMemo, useState } from "react";
 
 const FavouritesSection = () => {
     const { stats, isError, isFetchingFavourites, favouritedRecords } =
         FavouriteVM();
+
+    const router = useRouter();
+    const {
+        query: { q },
+    } = router;
+    const [currentPageNo, setCurrentPageNo] = useState<number>(
+        router.query.page ? parseInt(router.query.page as string) : 1
+    );
+    const [totalPages, setTotalPages] = useState(1);
+    const [pageSize, setPageSize] = useState(10);
+
+    // useEffect(() => {
+    //     if (currentPageNo.toString() != router.query?.page) {
+    //         router.replace({
+    //             query: { ...router.query, page: currentPageNo },
+    //         });
+    //     }
+    // }, [currentPageNo]);
+
+    useEffect(() => {
+        setTotalPages(
+            Math.ceil((favouritedRecords?.length as number) / pageSize)
+        );
+    }, [favouritedRecords]);
+
+    const currentFavouritesRecords = useMemo(() => {
+        if (!favouritedRecords) return [];
+        const startIndex = (currentPageNo - 1) * pageSize;
+        const endIndex = startIndex + pageSize;
+        return favouritedRecords.slice(startIndex, endIndex);
+    }, [currentPageNo, pageSize, favouritedRecords]);
 
     if (isError) {
         return (
@@ -33,7 +67,7 @@ const FavouritesSection = () => {
                 <InfoAlert message="No favourites found." className="mt-1" />
             )}
             <div className="flex flex-col" data-test-id="results table">
-                {favouritedRecords?.map((data) => (
+                {currentFavouritesRecords?.map((data) => (
                     <ResultCard
                         key={data.id}
                         data={data}
@@ -48,6 +82,11 @@ const FavouritesSection = () => {
                     />
                 ))}
             </div>
+            <Pagination
+                currentPage={currentPageNo}
+                totalPages={totalPages}
+                setPageNumber={setCurrentPageNo}
+            />
         </div>
     );
 };
