52e6e5ffca21a036eb72be9e66ec69b1e0f60e61
farzpal <farzpal@dtime.ai>
2023-12-14 13:15:08 +0530

    arrange datasets in org det page alphabetically

--- pages/api/datasets-by-data-host.page.ts
+++ pages/api/datasets-by-data-host.page.ts
@@ -3,9 +3,8 @@ import Http from "common/http";
 export default async function datasetsByDataHost(req: any, res: any) {
     try {
         const { orgUUID, pageNumber, orgDatasetsCount } = req.query;
-        console.log(orgUUID, pageNumber, orgDatasetsCount);
         const response = await Http.get(
-            `/v5/datasets/by-data-host/${orgUUID}?page_number=${pageNumber}&page_size=${orgDatasetsCount}`,
+            `/v5/datasets/by-data-host/${orgUUID}/sorted-by/title?page_number=${pageNumber}&page_size=${orgDatasetsCount}`,
             {
                 baseUrl: process.env.NEXT_PUBLIC_PUBLIC_API_ROOT,
                 extraHeaders: {
